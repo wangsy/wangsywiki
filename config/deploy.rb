a54f99e59500a00c8778cd095b2a02d62b586733
Sooyong Wang <wangsy@wangsy.com>
2012-12-03 00:13:25 +0900

    change remote server g.mintech.kr

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,12 @@
 set :application, "wangsywiki"
-set :repository,  "git@mintech.kr:wangsywiki.git"
+set :repository,  "git@g.mintech.kr:wangsywiki.git"
 
 role :web, "woz"                          # Your HTTP server, Apache/etc
 set :ssh_options, { :forward_agent => true }
 
-namespace :wiki do
+#namespace :wiki do
 	task :sync do
 		run "cd /home/wangsy/www/wangsywiki/; git pull 2>&1"
 		run "cd /home/wangsy/www/wangsywiki/; git push 2>&1"
 	end
-end
+#end
