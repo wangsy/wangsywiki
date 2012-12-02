0ec2675aa82c868b3685fbf55dfd28723913a4f5
Sooyong Wang <wangsy@wangsy.com>
2012-12-03 00:15:18 +0900

    change directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :ssh_options, { :forward_agent => true }
 
 #namespace :wiki do
 	task :sync do
-		run "cd /home/wangsy/www/wangsywiki/; git pull 2>&1"
-		run "cd /home/wangsy/www/wangsywiki/; git push 2>&1"
+		run "cd /home/wangsy/wangsywiki/; git pull 2>&1"
+		run "cd /home/wangsy/wangsywiki/; git push 2>&1"
 	end
 #end
